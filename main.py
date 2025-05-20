563b28bdb06a96325fb3fdb06d1f466c3d395b81
ahmedSheriffff <ahmed2110311@miuegypt.edu.eg>
2025-05-20 05:20:53 +0300

    w

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
+# Root route
 @app.get("/")
-async def home():
-    return {"message": "Success"}
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=3000)
+def root():
+    return {"message": "Welcome to the root route!"}
+
+# Route to display info for items
+@app.get("/items/")
+def items():
+    return {"message": "Welcome to the items route!"}
+
+# Route to display info for updating items
+@app.get("/items/update/")
+def update_item():
+    return {"message": "Welcome to the update item route!"}
